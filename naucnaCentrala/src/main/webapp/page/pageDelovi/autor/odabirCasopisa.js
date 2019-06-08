bfaa07d3d7f643793f0b45e6830d835c14eaa360
nishikori <nishikori94@users.noreply.github.com>
2019-06-08 13:17:26 +0200

    Publish

---
+++ naucnaCentrala/src/main/webapp/page/pageDelovi/autor/odabirCasopisa.js
@@ -0,0 +1,45 @@
+naucnaCentrala.controller('odabirCasopisa', [
+		'$scope',
+		'$state',
+		'$http',
+		function($scope, $state, $http) {
+
+			$scope.casopisi = [];
+			$scope.properties = [];
+			$scope.casopis={};
+
+			
+			$scope.logout = function() {
+				$rootScope.korisnik = "";
+				$state.go("login");
+			}
+			
+			$scope.pokreniProces = function() {
+				$http.get("http://localhost:8080/process/start").then(
+						function(response) {
+							$scope.properties = response.data;
+						}, function(response) {
+						});
+			};
+			
+			$scope.prikaziCasopise = function() {
+				$http.get("http://localhost:8080/casopis/sviCasopisi").then(
+						function(response) {
+							$scope.casopisi = response.data;
+
+						}, function(response) {
+						});
+			};
+
+			$scope.odaberiCasopis = function(x) {
+				$http.post("http://localhost:8080/casopis/odaberi/"+ $scope.properties.taskId,x)
+                    .then(function(response) {
+                        $state.go("prijavaRada",{"processId": $scope.properties.processInstanceId});
+				}).then(function(value) {
+
+				})
+			}
+
+			$scope.prikaziCasopise();
+			$scope.pokreniProces();
+		} ]);
